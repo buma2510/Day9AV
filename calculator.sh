2cc6d03ba4301ab3203a48d83825d5ea217e9c96
Ubuntu <ubuntu@ip-172-31-94-192.ec2.internal>
2024-11-27 12:03:44 +0000

    new calculator

--- calculator.sh
+++ calculator.sh
@@ -1,1 +1,16 @@
-x = a+b
+#!/bin/bash
+
+#Addition function 
+echo  "# Bumy Shell scripting ####"
+echo  "Author: Randy Munang ######"
+echo  "## Addition function ######"
+
+echo Perform Addition
+
+x=$((1+2))
+
+# or x=$(expr 1+2)
+
+echo "The result of 1+2 is $x"
+
+
---
+++ mycalculator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Addition function 
+echo "## Bumy Shell scripting ####"
+echo "# Author: Randy Munang ######"
+echo "## Addition function ######"
+
+# Accept user input
+read -p "Enter the first number (a): " a
+read -p "Enter the second number (b): " b
+
+# Perform addition
+x=$((a + b))
+
+# Print the result
+echo "The result of $a + $b is: $x"
+
